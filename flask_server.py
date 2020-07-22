fbae9a6143a3584efc4ed5035bb248931edc47fb
hono63 <hono6363@yahoo.co.jp>
2020-07-22 23:53:32 +0900

    add html

--- flask_server.py
+++ flask_server.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,10 +7,9 @@ def hello():
     name = "Hello Flask!!"
     return name
 
-@app.route('/good')
-def good():
-    name = "Good"
-    return name
+@app.route('/index', methods=["GET", "POST"])
+def index():
+    return render_template("index.html", name="namae")
 
 if __name__ == "__main__":
     app.run(debug=True)
