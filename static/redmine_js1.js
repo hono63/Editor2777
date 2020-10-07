fb9ad846ae522c423da597a186631cbc73c3ec20
hono63 <hono6363@yahoo.co.jp>
2020-10-08 00:13:18 +0900

    Redmine情報

--- static/redmine_js1.js
+++ static/redmine_js1.js
@@ -3,6 +3,16 @@
 $("head").append('<link href="https://unpkg.com/tabulator-tables@4.8.1/dist/css/tabulator_simple.min.css" rel="stylesheet">'); // シンプル
 //$("head").append('<link href="https://unpkg.com/tabulator-tables@4.8.1/dist/css/bootstrap/tabulator_bootstrap.min.css" rel="stylesheet">'); // Bootstrap3
 
+// Redmine情報。ajaxで取ってくることもできるけど、ページ読み込み遅くなりそうなのでとりあえず固定。
+var INFO ={
+ // /users.json を以下に貼り付け ("total_count","offset","limit" などは削除してね)
+"users":[{"id":1,"login":"hono63","admin":true,"firstname":"N","lastname":"H","mail":"dummy@gmail.com","created_on":"2020-09-23T10:49:34Z","last_login_on":"2020-10-07T15:00:36Z"}]
+, // /issue_statuses.json を以下に貼り付け
+"issue_statuses":[{"id":1,"name":"新規","is_closed":false},{"id":2,"name":"進行中","is_closed":false},{"id":3,"name":"解決","is_closed":false},{"id":4,"name":"フィードバック","is_closed":false},{"id":5,"name":"終了","is_closed":true},{"id":6,"name":"却下","is_closed":true}]
+, // /enumerations/issue_priorities.json を以下に貼り付け
+"issue_priorities":[{"id":1,"name":"低め","is_default":false,"active":true},{"id":2,"name":"通常","is_default":true,"active":true},{"id":3,"name":"高め","is_default":false,"active":true},{"id":4,"name":"急いで","is_default":false,"active":true},{"id":5,"name":"今すぐ","is_default":false,"active":true}]
+};
+
 var apiKey = "";
 $.get('/redmine/my/account').done(function(data){
     apiKey = $('#content > div.box > pre', $(data)).first().text();
