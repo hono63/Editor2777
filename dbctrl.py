4a5efeef3e033674f5f8e86124379ad47074b6fc
hono63 <hono63@penguin>
2021-01-16 16:28:19 +0900

    fix indent

--- dbctrl.py
+++ dbctrl.py
@@ -3,26 +3,26 @@
 from models import User, Spec, json_serializer
 
 class DBCtrl():
-	def __init__(self, db):
-		self.db = db
-		#self.db.init_app(app)
-		self.db.create_all()
-		# sample
-		self.add(User, {"name": "Junsuke"})
-		self.add(Spec, {"name": "Hyoushi dake"})
-	def get_json_list(self, model):
-		datalist = []
-		for instance in self.db.session.query(model):
-			datalist.append(json_serializer(model, instance))
-		return datalist
-	def add(self, model, a_json):
-		a_json2 = {}
-		for k,v in a_json.items():
-			if k in model.ColInt:
-				a_json2[k] = int(v)
-			elif k in model.ColStr:
-				a_json2[k] = str(v)
-		instance = model(**a_json2)
-		self.db.session.add(instance)
-		self.db.session.commit()
+    def __init__(self, db):
+        self.db = db
+        #self.db.init_app(app)
+        self.db.create_all()
+        # sample
+        self.add(User, {"name": "Junsuke"})
+        self.add(Spec, {"name": "Hyoushi dake"})
+    def get_json_list(self, model):
+        datalist = []
+        for instance in self.db.session.query(model):
+            datalist.append(json_serializer(model, instance))
+        return datalist
+    def add(self, model, a_json):
+        a_json2 = {}
+        for k,v in a_json.items():
+            if k in model.ColInt:
+                a_json2[k] = int(v)
+            elif k in model.ColStr:
+                a_json2[k] = str(v)
+        instance = model(**a_json2)
+        self.db.session.add(instance)
+        self.db.session.commit()
 
--- models.py
+++ models.py
@@ -2,31 +2,31 @@
 from appdb import db
 
 def json_serializer(model, instance):
-	jdata = {}
-	for k,v in model.__dict__.items():
-		#print(jdata)
-		if k in model.ColInt:
-			jdata[k] = getattr(instance, k)
-		elif k in model.ColStr:
-			jdata[k] = getattr(instance, k)
-	return jdata
+    jdata = {}
+    for k,v in model.__dict__.items():
+        #print(jdata)
+        if k in model.ColInt:
+            jdata[k] = getattr(instance, k)
+        elif k in model.ColStr:
+            jdata[k] = getattr(instance, k)
+    return jdata
 
 class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String)
-	ColInt = ("id")
-	ColStr = ("name")
-	def __repr__(self):
-		return '<User %s>' % self.name
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ColInt = ("id")
+    ColStr = ("name")
+    def __repr__(self):
+        return '<User %s>' % self.name
 
 class Spec(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String)
-	definition = db.Column(db.String)
-	guid = db.Column(db.String)
-	userid = db.Column(db.Integer)
-	ColInt = ("id", "userid")
-	ColStr = ("name", "guid", "definition")
-	def __repr__(self):
-		return '<Spec %s>' % self.name
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    definition = db.Column(db.String)
+    guid = db.Column(db.String)
+    userid = db.Column(db.Integer)
+    ColInt = ("id", "userid")
+    ColStr = ("name", "guid", "definition")
+    def __repr__(self):
+        return '<Spec %s>' % self.name
 
