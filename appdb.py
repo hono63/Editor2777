3cbcdac9fef9eb7cbfaa515b77b66a87a66c5902
hono63 <hono63@penguin>
2021-01-16 16:43:36 +0900

    fix indent

--- appdb.py
+++ appdb.py
@@ -2,9 +2,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask("Editor2777")
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.sqlite3"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
--- flask_server.py
+++ flask_server.py
@@ -13,13 +13,12 @@ dbc = DBCtrl(db)
 @app.route('/')
 def route():
     #return redirect("index")
-    return "hello"
+    return "<h1>hello</h1>"
 
 @app.route('/index', methods=["GET", "POST"])
 def index():
     return render_template("index.html", users=dbc.get_json_list(User), specs=dbc.get_json_list(Spec))
 
 if __name__ == "__main__":
-    print("hello")
-    app.run(host="127.0.0.1", port=2777, debug=True)
+    app.run(host="127.0.0.1", port=2777, debug=False)
 
