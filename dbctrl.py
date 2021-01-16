80408f30a28f33164b47d6171face8ee1124a52c
hono63 <hono6363@yahoo.co.jp>
2021-01-16 21:05:16 +0900

    table edit feature

--- dbctrl.py
+++ dbctrl.py
@@ -1,6 +1,7 @@
 # データベースの操作を行う
 
 from models import User, Spec, json_serializer
+import uuid
 
 class DBCtrl():
     def __init__(self, db):
@@ -16,13 +17,30 @@ class DBCtrl():
             datalist.append(json_serializer(model, instance))
         return datalist
     def add(self, model, a_json):
-        a_json2 = {}
-        for k,v in a_json.items():
-            if k in model.ColInt:
-                a_json2[k] = int(v)
-            elif k in model.ColStr:
-                a_json2[k] = str(v)
-        instance = model(**a_json2)
+        if "id" in a_json:
+            # 更新
+            instance = self.db.session().query(model).filter(model.id == a_json["id"]).first()
+            for k,v in a_json.items():
+                if k in model.ColInt and v != "":
+                    setattr(instance, k, int(v))
+                elif k in model.ColStr:
+                    setattr(instance, k, str(v))
+            instance.update_cb()
+        else:
+            # 新規追加
+            a_json2 = {}
+            for k,v in a_json.items():
+                if k in model.ColInt:
+                    a_json2[k] = int(v)
+                elif k in model.ColStr:
+                    a_json2[k] = str(v)
+            instance = model(**a_json2)
+            instance.add_cb()
         self.db.session.add(instance)
+        self.db.session.commit()
+        return json_serializer(model, instance)
+    def delete(self, model, id):
+        instance = self.db.session.query(model).filter(model.id == id).first()
+        self.db.session.delete(instance)
         self.db.session.commit()
 
--- flask_server.py
+++ flask_server.py
@@ -2,13 +2,16 @@
 Run Editor server by using Flask
 """
 
+import json
+import copy
 #from npito import open_folder
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, jsonify
 from appdb import app, db
 from dbctrl import DBCtrl
 from models import User, Spec
 
 dbc = DBCtrl(db)
+MODELNAME = {"User": User, "Spec": Spec}
 
 @app.route('/')
 def route():
@@ -24,11 +27,19 @@ def index():
 
 @app.route('/update-data', methods=["POST"])
 def update_data():
-    if request.form:
-    userslist = dbc.get_json_list(User)
-    specslist = dbc.get_json_list(Spec)
-    #print(userslist)
-    return render_template("index.html", users=userslist, specs=specslist)
+    status = 200
+    upd_data = {}
+    #json_data = json.loads(request.data.decode('utf-8'))
+    json_data = copy.copy(request.form)
+    model = MODELNAME[json_data["modelname"]]
+    if "flag_addupd" in json_data:
+        upd_data = dbc.add(model, json_data) # add or update
+    elif "flag_delete" in json_data:
+        dbc.delete(model, json_data["id"])
+        upd_data = {"id": json_data["id"]}
+    else:
+        status = 400
+    return jsonify({"updated_data": upd_data}), status
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=2777, debug=True)
